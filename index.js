c5f83aad5266898dbaf3e5787c255da0b76e450f
NimmBle <icancube96@gmail.com>
2023-10-03 09:34:12 +0300

    working, structured

--- index.js
+++ index.js
@@ -6,39 +6,13 @@ function PriceInJar(url) {
     const symbol = '.';
     const className = '.price';
     method(url, symbol, className)
-    // x(url, '.price')(function(err, price) {  
-    //     var PriceAsArray = price.split('');
-    //     const Price = [];
-    //     for (let i = 0; i < PriceAsArray.length; i++) {
-    //         var element = PriceAsArray[i];
-    //         if (element != '.')
-    //             Price.push(element);
-    //         else 
-    //             break;
-    //     }
-    //     var FinalPrice = Number(Price.join(''));
-    //     console.log(FinalPrice);
-    // })
 }
 function PriceInEmag(url) {
     const symbol = ',';
     const className = '.product-new-price';
     method(url, symbol, className)
-    // x(url, '.product-new-price')(function(err, price) {
-    //     var PriceAsArray = price.split('');
-    //     const Price = [];
-    //     for (let i = 0; i < PriceAsArray.length; i++) {
-    //         var element = PriceAsArray[i];
-    //         if (element != ',')
-    //             Price.push(element);
-    //         else 
-    //             break;
-    //     }
-    //     var FinalPrice = Number(Price.join(''));
-    //     console.log(FinalPrice);
-    // })
 }
-function PriceInTehnopolis() {
+function PriceInTehnopolis(url) {
     const symbol = '.';
     const className = '.price-value';
     method(url, symbol, className)
@@ -47,8 +21,6 @@ function method(url, symbol, className) {
     x(url, className)(function(err, price) {
         var PriceAsArray = price.replace(/\s/g,'').split('');
         const Price = [];
-        console.log(typeof PriceAsArray)
-        console.log(typeof Price)
         for (let i = 0; i < PriceAsArray.length; i++) {
             var char = PriceAsArray[i];
             if (char != symbol)
@@ -60,25 +32,6 @@ function method(url, symbol, className) {
         console.log(FinalPrice);
     })  
 }
-// function PriceInXiaomiBulgaria(url) {
-//     x(url, '.product_title')(function(err, price) {
-//         console.log(price);
-//     })
-// }
-// async function banana() {
-//     const response = await axios.get("https://www.ozone.bg/product/igri-shah-tabla-ludo-ne-se-sardi-choveche-zmii-i-stalbi/")
-//     console.log(response.body)
-// }
-// banana();
-// async function banana() {
-//     const response = await axios({
-//         method: 'get',
-//         url: 'https://mi-bulgaria.com/buy/xiaomi-smart-band-8/',
-//         transformResponse: [function (data) {
-//             const $ = cheerio.load(data.body)
-//             console.log($('.woocommerce-Price-amount').text());
-//         }],
-//     });
-// }
-// banana();
-PriceInEmag
+PriceInJar('https://www.jarcomputers.com/Samsung-EB-P3400XUEGEU_prod_EBACCSAMSUNGEBP3400XUEGEUS.html')
+PriceInEmag('https://www.emag.bg/smartfon-samsung-galaxy-a14-64gb-4gb-ram-4g-silver-sm-a145rzsueue/pd/DQPGV6MBM/')
+PriceInTehnopolis('https://www.technopolis.bg/bg/Smartfoni-i-mobilni-telefoni/Smartfon-GSM-APPLE-IPHONE-15-YELLOW/p/502074');
